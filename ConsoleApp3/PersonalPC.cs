10d4c670b803a9fd17ddce453945f5338dd9d7e9
Никита <153456515+Lairont@users.noreply.github.com>
2025-02-04 19:14:18 +0200

    Add files via upload

---
+++ ConsoleApp3/PersonalPC.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class PersonalPC
+    {
+        public string Model;
+        public double CPUfrequency;
+        public int RAMcapacity;
+        public int HDDcapacity;
+        /// <summary>
+        /// конструктор без параметров
+        /// </summary>
+        public PersonalPC()
+        {
+
+        }
+        /// <summary>
+        /// конструктор с одним параметром 
+        /// </summary>
+        /// <param name="Model"></param>
+        public PersonalPC(string Model)
+        {
+            this.Model = Model;
+        }
+        /// <summary>
+        /// конструктор с двумя параметрами
+        /// </summary>
+        /// <param name="Model"></param>
+        /// <param name="CPUfrequency"></param>
+        public PersonalPC(string Model, double CPUfrequency) : this(Model)
+        {
+            this.CPUfrequency = CPUfrequency;
+        }
+        /// <summary>
+        /// конструктор с тремя параметрами
+        /// </summary>
+        /// <param name="Model"></param>
+        /// <param name="CPUfrequency"></param>
+        /// <param name="RAMcapacity"></param>
+        public PersonalPC(string Model, double CPUfrequency, int RAMcapacity) : this(Model, CPUfrequency)
+        {
+            this.RAMcapacity = RAMcapacity;
+        }
+        /// <summary>
+        /// конструктор с четырьмя параметрами
+        /// </summary>
+        /// <param name="Model"></param>
+        /// <param name="CPUfrequency"></param>
+        /// <param name="RAMcapacity"></param>
+        /// <param name="HDDcapacity"></param>
+        public PersonalPC(string Model, double CPUfrequency, int RAMcapacity, int HDDcapacity) : this(Model, CPUfrequency, RAMcapacity)
+        {
+            this.HDDcapacity = HDDcapacity;
+        }
+        /// <summary>
+        /// Метод, котрый выводит информацию о персональном пк 
+        /// </summary>
+        public void Info()
+        {
+            Console.WriteLine($"Модель: {Model}, Частота процессора: {CPUfrequency} ГГц," +
+                $" Обьем ОЗУ: {RAMcapacity} ГБ, Обьем жесткого диска: {HDDcapacity} ГБ");
+        }
+        public override string ToString()
+        {
+            return $"Модель: {Model}, Частота процессора: {CPUfrequency} ГГц," +
+                $" Обьем ОЗУ: {RAMcapacity} ГБ, Обьем жесткого диска: {HDDcapacity} ГБ";
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        /// <summary>
+        /// основной метод кода
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            PersonalPC pc1 = new PersonalPC();
+            pc1.Info();
+            Console.WriteLine($"{pc1.ToString()}");
+            Console.WriteLine();
+            PersonalPC pc2 = new PersonalPC("C1");
+            pc2.Info();
+            Console.WriteLine($"{pc2.ToString()}");
+            Console.WriteLine();
+            PersonalPC pc3 = new PersonalPC("C1",3.5);
+            pc3.Info();
+            Console.WriteLine($"{pc3.ToString()}");
+            Console.WriteLine();
+            PersonalPC pc4 = new PersonalPC("C1",3.5,16);
+            pc4.Info();
+            Console.WriteLine($"{pc4.ToString()}");
+            Console.WriteLine();
+            PersonalPC pc5 = new PersonalPC("C1",3.5,16,2000);
+            pc5.Info();
+            Console.WriteLine($"{pc5.ToString()}");
+            Console.WriteLine();
+            Console.Write("Модель: ");
+            string Model = Console.ReadLine();
+            Console.Write("Частота процессора: ");
+            double CPUfrequency = double.Parse(Console.ReadLine());
+            Console.Write("Обьем оперативной памяти: ");
+            int RAMcapacity = int.Parse(Console.ReadLine());
+            Console.Write("Обьем жесткого диска:");
+            int HDDcapacity = int.Parse(Console.ReadLine());
+            PersonalPC pc6 = new PersonalPC(Model, CPUfrequency, RAMcapacity, HDDcapacity);
+            pc6.Info();
+            Console.WriteLine($"{pc6.ToString()}");
+            Console.ReadLine();
+
+        }
+    }
+}
