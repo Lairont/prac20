329e139bd72fd300682f5563aa2967d586670a49
Никита <153456515+Lairont@users.noreply.github.com>
2025-02-04 19:13:28 +0200

    Add files via upload

---
+++ ConsoleApp2/Build.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    /// <summary>
+    /// класс проекта строения
+    /// </summary>
+    internal class Build
+    {
+        /// <summary>
+        /// поле названия строения
+        /// </summary>
+        public string name;
+        /// <summary>
+        /// поле площади
+        /// </summary>
+        public double area;
+        /// <summary>
+        /// поле количества жильцов
+        /// </summary>
+        public int kvo;
+        /// <summary>
+        /// поле количества этажей
+        /// </summary>
+        public int floor;
+        /// <summary>
+        /// конструктор без параметров
+        /// </summary>
+        public Build() 
+        { }
+        /// <summary>
+        /// конструктор с одним параметром 
+        /// </summary>
+        /// <param name="name"></param>
+        public Build(string name)
+        {
+            this.name = name; 
+        }
+        /// <summary>
+        /// конструктор с двумя параметрами
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="area"></param>
+        public Build(string name, double area) : this(name)
+        {
+            this.area = area;
+        }
+        /// <summary>
+        /// конструктор с тремя параметрами
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="area"></param>
+        /// <param name="kvo"></param>
+        public Build(string name, double area, int kvo) : this(name, area)
+        {
+            this.kvo = kvo;
+        }
+        /// <summary>
+        /// конструктор с четырьмя параметрами
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="area"></param>
+        /// <param name="kvo"></param>
+        /// <param name="floor"></param>
+        public Build(string name, double area, int kvo, int floor): this(name, area, kvo)
+        {
+            this.floor = floor;
+        }
+        /// <summary>
+        /// метод, который выводит информацию о строении 
+        /// и вычисляет площадь на одного жильца
+        /// </summary>
+        public void ShowInfo()
+        {
+            Console.WriteLine($"В доме {name} площадью {area} кв. метров живет {kvo} человек," +
+                $" на человека - {area / kvo:f2} кв. метров");
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        /// <summary>
+        /// основной метод кода
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Build dom1 = new Build();
+            dom1.ShowInfo();
+            Console.WriteLine();
+            dom1.name = "Дача";
+            dom1.area =600.3;
+            dom1.kvo = 3;
+            dom1.ShowInfo();
+            Console.WriteLine();
+            Build dom2 = new Build();
+            dom2.name = "Отель";
+            dom2.area = 6000.3;
+            dom2.kvo = 13450;
+            dom2.ShowInfo();
+            Console.WriteLine();
+            Console.Write("Название строения: ");
+            string name=Console.ReadLine();
+            Console.Write("Площадь строения: ");
+            double area = double.Parse(Console.ReadLine());
+            Console.Write("Количество жильцов: ");
+            int kvo = int.Parse(Console.ReadLine());
+            Console.Write("Количество этажей:");
+            int floor = int.Parse(Console.ReadLine());
+            Build dom3 = new Build(name, area, kvo, floor);
+            dom3.ShowInfo();
+            Console.Read();
+
+
+        }
+    }
+}
